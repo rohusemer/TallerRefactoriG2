020fab7cff0ec5557db61a09d3473385cf774a6f
Briggitte Lopez <brililop@espol.edu.ec>
2020-08-13 18:25:37 -0500

    Code Smells:Duplicate Code y Refactoring: Subir Campo

---
+++ TalllerRefactoring/src/modelos/Persona.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bllv1
+ */
+public class Persona {
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String direccion;
+    public String telefono;
+    public ArrayList<Paralelo> paralelos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public ArrayList<Paralelo> getParalelos() {
+        return paralelos;
+    }
+
+    public void setParalelos(ArrayList<Paralelo> paralelos) {
+        this.paralelos = paralelos;
+    }
+    
+    
+}
