1eee3e2b6c10a2969b3fe7bb8f9a176616fef4e6
Lea Cordova <lealcord@espol.edu.ec>
2020-08-13 21:09:00 -0500

    .

--- TalllerRefactoring/src/modelos/Materia.java
+++ TalllerRefactoring/src/modelos/Materia.java
@@ -1,15 +1,9 @@
 package modelos;
 
-public class Materia {
+public class Materia extends Notas{
     public String codigo;
     public String nombre;
     public String facultad;
-    public double notaInicial;
-    public double notaFinal;
-    public double notaTotal;
-    public double notaExamen;
-    public double notaDeberes;
-    public double notaLecciones;
-    public double notaTalleres;
+    
     
 }
---
+++ TalllerRefactoring/src/modelos/Notas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author bllv1
+ */
+public class Notas {
+    public double notaInicial;
+    public double notaFinal;
+    public double notaTotal;
+    public double notaExamen;
+    public double notaDeberes;
+    public double notaLecciones;
+    public double notaTalleres;
+}
